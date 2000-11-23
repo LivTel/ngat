f1118278907c96d1f7f0649fddb4897853fac3c6
Steve Fraser <snf@example.com>
2000-11-23 11:38:47 +0000

    Initial revision

---
+++ ngat/phase2/DBObject.java
@@ -0,0 +1,142 @@
+package ngat.phase2;
+
+import ngat.phase2.nonpersist.*;
+import java.util.*;
+import java.io.*;
+
+/** 
+ * A top level object from which all database objects are derived. Contains
+ * generic variables and methods.
+ * $Id: DBObject.java,v 1.1 2000-11-23 11:38:08 snf Exp $
+ */
+public class DBObject implements Serializable {
+
+    /** The unique (at a given subtree level) name/ident for this object. */
+    protected String name;
+
+    /** The unique path expression for this object. A string made up from
+     * the names of all its ancestors concatenated in order.
+     * <br>
+     * <pre>
+     * e.g. Root/Tag1/User3 for a particular user's Proposals.
+     * <pre>
+     */
+    protected String path;
+
+    /** A PRBS generated key needed to unlock this object. It is upto the
+     * client application to ensure the key it generates is <i>sufficiently random</i>.*/
+    protected int lock;
+
+    /** A generic scheduling coefficient for the object. These are used in
+     * different ways by different objects, if at all. */
+    protected float schedCoeff;
+
+    /** Denotes whether the object is marked for deletion at a later stage. */
+    protected boolean deleted;
+
+    /** Construct a generic object with the name 'untitled'. */
+    public DBObject() { this("untitled");}
+
+    /** Construct a generic object with specified name. */
+    public DBObject(String name) {
+	this.name = name;
+	this.path = "";
+	this.schedCoeff = 1.0f;
+	this.lock = 0;
+	this.deleted = false;
+    }
+
+    /** Construct a DBObject from the supplied NonPersistent DBObject. */
+    public DBObject(NPDBObject npo) {
+	name = npo.getName();
+	path = npo.getPath();
+	schedCoeff = npo.getSchedCoeff();
+	lock = npo.getLock();
+	deleted = false;
+    }
+
+    
+    /** Translate the DBObject into a NonPersistent mirror object.
+     * This method just pushes relevant stuff into an existing NPDBObject. */
+    public void stuff(NPDBObject npo) {
+	npo.setName(name);
+	npo.setPath(path);
+	npo.setSchedCoeff(schedCoeff);
+	npo.setLock(lock);
+    }
+
+    /** Translate to NP, creating a new NPDBObject. */
+    public NPDBObject makeNP() {
+	NPDBObject npo = new NPDBObject();
+	stuff(npo);
+	return npo;
+    }
+
+    // Database locking methods.
+
+    /** Set the Lock for this object. 
+     * @return false if alreadylocked with a different key. */
+    public boolean setLock(int key) { 
+	boolean set = true;   
+	if (isLocked() && key != getLock()) set = false;
+	this.lock = key;
+	return set;
+    }
+    
+    /** Unlock this object using the specified key.
+     * @return false if locked with a different key. */
+    public boolean unLock(int key) {
+	boolean freed = true;   
+	if (isLocked() && key != getLock()) freed = false;
+	this.lock = 0;
+	return freed;
+    }
+
+    /** Determine whether the object is locked. */
+    public boolean isLocked() { return (lock != 0);}
+
+    /** Get the key for this object's lock.*/
+    public int getLock() { return lock;}
+
+    // Generic Methods.
+
+    /** Get the object's identity. */
+    public String getName() { return name;}
+    
+    /** Set the Object's name (used by stuffers). */
+    public void setName(String name) { this.name = name;}
+
+    public void setId(String name) { this.name = name;}
+
+    public String getId() { return name;}
+
+    /** Set the object's path - when placing in a tree. */
+    public void setPath(String path) {this.path = path;}
+    
+    /** Get the object's path expression - ie. its tree location. */
+    public String getPath() { return path;}
+
+    /** Get the full path anme for this Object. */
+    public String getFullPath() { return path+"/"+name;}
+
+    /** Get the object's schedule coefficient. */
+    public float getSchedCoeff() { return schedCoeff;}
+
+    /** Set the object's schedule coefficient. */
+    public void setSchedCoeff(float schedCoeff) {this.schedCoeff = schedCoeff;}
+
+    /** Determine whether the object is marked for deletion. */
+    public boolean deleted() { return deleted;}
+
+    /** Mark the object for deletion - the time of deletion is not known
+     * in advance, a future implementation may allow a period of grace to be specified.*/
+    public void setDeleted(boolean deleted) { this.deleted = deleted;}
+
+}
+
+/** $Log: not supported by cvs2svn $
+/** Revision 1.1  2000/05/25 10:14:41  snf
+/** Initial revision
+/** */
+
+
---
+++ ngat/phase2/Root.java
@@ -0,0 +1,253 @@
+package ngat.phase2;
+import ngat.phase2.nonpersist.*;
+
+import com.odi.*;
+import com.odi.util.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.io.*;
+
+
+/**
+ * The entry point for the Phase II database. All objects in the
+ * database tree hang off this (singleton) object.
+ * <br>
+ * $Id: Root.java,v 1.1 2000-11-23 11:38:47 snf Exp $
+ *
+ */
+public class Root extends DBObject implements Serializable {
+
+     // Variables.
+
+    /** The list of TAGs associated with this database. */
+    protected OSHashMap tags;
+    
+    // Constructor.
+    
+    /** Create a database Root with the name 'untitled'. */
+    public Root() {this("untitled");}
+    
+    /** Create a database Root with the specified (database) name. */
+    public Root(String name) {
+	super(name);
+	tags = new OSHashMap();
+    }
+
+    // Accessors.
+    // There are no additional accessors.
+
+    // Descendant Mutators.
+    
+    // Tag Methods.
+
+    /** Add the specified TAG to the database Root.*/
+    public void addTag(Tag tag) {
+	tags.put(tag.getName(), tag);
+	tag.setPath(getPath() + "/" + getName());
+    }
+    
+    /** Remove the specified TAG from the database Root.*/
+    public void deleteTag(Tag tag) {
+	tags.remove(tag.getName());
+    }
+
+    /** Remove ALL TAGs from the database Root.*/
+    public void removeAllTags() {
+	tags.clear();
+    }
+    
+    /** Return an Iterator over the list of TAGs.*/
+    public Iterator listAllTags() {
+	return tags.values().iterator();
+    }
+    
+    /** Locate the record of the TAG with the given name or null if not found.*/
+    public Tag findTag(String name) {
+	if (tags.containsKey(name)) return ((Tag)tags.get(name));
+	return null;
+    }
+
+    /** Return a reference to the list of TAGs.*/
+    public OSHashMap getTags() { return tags;}
+    
+    // NP -> P Translator.
+    
+    /** Translate a NonPersistent NPRoot into a Root including all descendants.*/
+    public Root(NPRoot npRoot) {
+	super(npRoot);
+	Iterator it;
+	
+	// Recursively call Daughter Translators.
+	
+	tags = new OSHashMap();
+	it = npRoot.listAllNPTags();
+	while (it.hasNext()) {
+	    try {
+		NPTag npTag = (NPTag)it.next();
+		Class npClazz = npTag.getClass();
+		String npName = npClazz.getName();
+		int k = npName.indexOf("nonpersist.NP");
+		String pName = npName.substring(0,k).concat(npName.substring(k+13));
+		Class pClazz = Class.forName(pName);
+		Constructor pCon = pClazz.getConstructor(new Class[]{npClazz});
+		Tag tag = (Tag)pCon.newInstance(new Object[]{npTag});
+		addTag(tag);
+	    } catch (ClassNotFoundException re1){
+		System.out.println("Translation Error: "+re1);
+	    } catch (NoSuchMethodException re2) {
+		System.out.println("Translation Error: "+re2);
+	    } catch (InvocationTargetException re3) {
+		System.out.println("Translation Error: "+re3);
+	    } catch (IllegalAccessException re4) {
+		System.out.println("Translation Error: "+re4);
+	    } catch (InstantiationException re5) {
+		System.out.println("Translation Error: "+re5);
+	    }
+	}
+    } // end (NP -> P Translator).
+    
+    // P -> NP Translator. 
+    /** When translating to NPRoot, stuff all the internal fields and descendants.*/
+    public void stuff(NPRoot npRoot) {
+	super.stuff(npRoot);
+	Iterator it;
+	it = listAllTags();
+	while (it.hasNext()) {
+	    npRoot.addNPTag((NPTag)(((Tag)it.next()).makeNP()));
+	}
+    } // end (P -> NP Translator).
+    
+    // P -> NP Translator.   
+    /** Make this Root into an NPRoot.*/
+    public NPDBObject makeNP() {
+	NPRoot npRoot = new NPRoot();
+	stuff(npRoot);
+	return npRoot;
+    } // end (makeNp).
+    
+    // Self Lock Method.
+    
+    /** Lock this database Root) and recursively all descendants with the supplied key,*/
+    public void lock(int key) {
+	if (canLock()) {
+	    lockChildren(key);
+	}
+    } // end (lock).
+
+    // Child Update Method.
+    /** Reset paths for descendants to reflect movement of this Root?.*/
+    public void updateChildren() {
+	Iterator it  = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    tag.setPath(path+"/"+name);
+	    tag.updateChildren();
+	}
+    } // end (updateChildren).
+    
+    // Child Lock-Check Method.
+    /** Return true if this Root and ALL descendants are unlocked.*/
+    public boolean canLock() {
+	if (isLocked()) return false;
+	Iterator it  = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    if (!tag.canLock()) return false;
+	}
+	return true;
+    } // end (canLock).
+    
+    // Child Locking Method.
+    /** Lock all descendants using the supplied key.*/
+    public void lockChildren(int key) {
+	Iterator it  = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    tag.lockChildren(key);
+	}
+	setLock(key);
+    } // end (lockChildren).
+
+    // Child Un-Locking Method.
+    /** Unlock this (Root) and all descendants using the supplied key if possible.
+     * Returns silently if it  fails. Use canLock() to see if the unlock succeeded.*/
+    public void unLockChildren(int key) {
+	Iterator it  = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    tag.unLockChildren(key);
+	}
+	unLock(key);
+    } // end (unLockChildren).
+    
+    // Force Un-Locking Method.
+    /** Forcibly unlock this (Root) and all descendants whatever their current key.*/
+    public void forceUnLock() {
+	lock = 0;
+	Iterator it  = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    tag.forceUnLock();
+	}
+    } // end (forceUnLock).
+
+    // FixedGroup Method.
+    /** Return the next fixed group available ## DEPRECATED.*/
+    public FixedGroup getNextFixedGroup() {
+	FixedGroup earliest = new FixedGroup("NULL");
+	// Set earliest Fixed to about Y29,688 AD !
+	earliest.setFixedTime(999999999999999L);
+	Iterator it = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    if (!tag.isLocked() && !tag.deleted()) {
+		FixedGroup fg = tag.getNextFixedGroup();
+		if (fg != null) {
+		    if (fg.getFixedTime() < earliest.getFixedTime()) {
+			earliest = fg;
+		    }
+		}
+	    }
+	}
+	return earliest;
+    } // end (getNextFixedGroup).
+    
+    // Scheduling Method.
+    /** Invoke the scheduling function on this database. Calls recursively 
+     * through descendants until all objects which contribute to scheduling 
+     * descisions have been visited.*/
+    public ScheduleDescriptor schedule() {
+	ScheduleDescriptor best = new ScheduleDescriptor(null,0.0f,0);
+	Iterator it = listAllTags();
+	while (it.hasNext()) {
+	    Tag tag = (Tag)it.next();
+	    if (!tag.isLocked() && !tag.deleted()) {
+		ScheduleDescriptor sd = tag.schedule();
+		if (sd.getGroup() != null) {
+		    if (sd.getScore() > best.getScore()) {
+			best.setGroup(sd.getGroup());
+			best.setScore(sd.getScore());
+			best.setExecTime(sd.getExecTime());
+		    }
+		}
+	    }
+	}
+	return applyScheduling(best);
+    } // end (schedule).
+    
+    // Scheduling Algorithm.
+    /** Include any Root specific schedule weighting with this method.
+     * Currently this does nothing.
+     * @param best The 'best' schedule so far.
+     * @return The modified schedule.*/
+    public ScheduleDescriptor applyScheduling(ScheduleDescriptor best) {      	
+	// Insert code here to apply this.object's schedule coefft.
+	// will make use of attribute: [ schedCoeff ] and possibly
+	// various ServerContext and TelescopeEnvironment variables.	
+	best.setScore(best.getScore()*schedCoeff);
+	return best;	
+    } // end (applyScheduling).
+    
+} // end class def [Root].
+
+/** $Log: not supported by cvs2svn $ */
