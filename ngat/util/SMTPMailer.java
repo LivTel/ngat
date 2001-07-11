72753b119d8962c8e769d30939f53273589e21dc
Steve Fraser <snf@example.com>
2001-07-11 10:24:26 +0000

    Changed params.

--- ngat/util/SMTPMailer.java
+++ ngat/util/SMTPMailer.java
@@ -10,46 +10,32 @@ import ngat.phase2.*;
 /** Test class for automatically mailing PI when a Proposal is inserted in the PhaseII database.*/
 public class SMTPMailer {
 
+    /** Message text.*/
+    protected String message;
+
+    /** E-mail address for receiver.*/
     protected String recipient;
-
-    protected Proposal proposal;
-
-    public SMTPMailer(String recipient, Proposal proposal, String password) {
+    
+    public SMTPMailer(String recipient, String message) {
+	this.message   = message;
 	this.recipient = recipient;
-	this.proposal = proposal;
 	String line ="";
 	try {
 	    Socket sock = new Socket("ltccd1",25);
-	    BufferedInputStream in = new BufferedInputStream(sock.getInputStream());
+	    BufferedInputStream  in  = new BufferedInputStream(sock.getInputStream());
 	    BufferedOutputStream out = new BufferedOutputStream(sock.getOutputStream());
 	    recv(in);
-	    send(out,"MAIL FROM: OSS_Server@ltccd1.livjm.ac.uk\r\n");
+	    send(out,"MAIL FROM: RoboticControl@ltccd1.livjm.ac.uk\r\n");
 	    line = recv(in);
 	    send(out,"RCPT TO:"+recipient+"\r\n");
 	    line = recv(in);
 	    send(out,"DATA\r\n");
 	    line = recv(in);
-	    send(out,"subject:Your Proposal ["+proposal.getName()+"]\r\n\r\n");
-	    send(out,"** OSS Server automatically generated notification **\r\n\r\n");
-	    send(out,"** Your Proposal ["+proposal.getName()+"] has been entered and is now ready for editing **\r\n\r\n");
-	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-	    String dateString = sdf.format(new Date(proposal.getActivationDate()));
-	    send(out,"** The Proposal will become active for scheduling on "+dateString+" **\r\n\r\n");
-	    send(out,"** The total allocated time is: "+proposal.getAllocatedTime()+" hrs **\r\n\r\n");
-	    send(out,"** Divided as follows:**\r\n\r\n");
-	    send(out,"** Bright Moon /  Good Seeing: "+proposal.getAllocatedFraction_BX()+" **\r\n\r\n");
-	    send(out,"** Bright Moon / Avge Seeing: "+proposal.getAllocatedFraction_BA()+" **\r\n\r\n");
-	    send(out,"** Bright Moon / Poor Seeing: "+proposal.getAllocatedFraction_BP()+" **\r\n\r\n");
-	    send(out,"** Dark Moon /  Good Seeing: "+proposal.getAllocatedFraction_DX()+" **\r\n\r\n");
-	    send(out,"** Dark Moon / Avge Seeing: "+proposal.getAllocatedFraction_DA()+" **\r\n\r\n");
-	    send(out,"** Dark Moon / Poor Seeing: "+proposal.getAllocatedFraction_DP()+" **\r\n\r\n");	
-	    send(out,"*********************************************\r\n\r\n");
-	    send(out,"** The initial password is \""+password+"\" **\r\n\r\n");
-	    send(out,"** You should change this before you start editing **\r\n\r\n");
-	    send(out,"*********************************************\r\n\r\n");
-	    send(out,"** THIS IS A TEST MESSAGE ** \r\n\r\n");
-	    send(out,".\r\n");
-	    line = recv(in);
+	    send(out,"subject:mobile\r\n\r\n");
+	    send(out,"** From Robotic Controller automatically generated notification **\r\n\r\n");	   
+	    send(out,"** MESSAGE -"+message+"\r\n\r\n");
+	    send(out,".\r\n"); 
+	    line = recv(in); 
 	} catch (IOException ie) {
 	    System.out.println("Error: "+ie);
 	    return;
