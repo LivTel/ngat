746623ed231f1f0cd36798df243afe770241a2b6
Steve Fraser <snf@example.com>
2002-09-25 10:44:56 +0000

    backup

--- ngat/util/SMTPMailer.java
+++ ngat/util/SMTPMailer.java
@@ -14,11 +14,11 @@ public class SMTPMailer {
     protected String message;
 
     /** E-mail address for receiver.*/
-    protected String recipient;
+    protected String email;
     
-    public SMTPMailer(String recipient, String message) {
-	this.message   = message;
-	this.recipient = recipient;
+    public SMTPMailer(String email, String name, String from, String subject, String message) {
+	this.message = message;
+	this.email   = email;
 	String line ="";
 	try {
 	    Socket sock = new Socket("ltccd1",25);
@@ -27,15 +27,17 @@ public class SMTPMailer {
 	    recv(in);
 	    send(out,"MAIL FROM: RoboticControl@ltccd1.livjm.ac.uk\r\n");
 	    line = recv(in);
-	    send(out,"RCPT TO:"+recipient+"\r\n");
+	    send(out,"RCPT TO:"+email+"\r\n");
 	    line = recv(in);
 	    send(out,"DATA\r\n");
 	    line = recv(in);
-	    send(out,"subject:mobile\r\n\r\n");
-	    send(out,"** From Robotic Controller automatically generated notification **\r\n\r\n");	   
-	    send(out,"** MESSAGE -"+message+"\r\n\r\n");
+	    send(out,"From: <"+from+"> RoboticControl@ltccd1.livjm.ac.uk"+"\r\n");
+	    send(out,"Operator: "+name+"\r\n");
+	    send(out,"Subject: "+subject+"\r\n\r\n");
+	    send(out,"** Robotic Controller automatically generated notification **\r\n");	   
+	    send(out,"** MESSAGE -"+message+"\r\n");
 	    send(out,".\r\n"); 
-	    line = recv(in); 
+	    //line = recv(in); 
 	} catch (IOException ie) {
 	    System.out.println("Error: "+ie);
 	    return;
