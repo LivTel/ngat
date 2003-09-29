f8059673692e1c6e7a358af511731c31e88ed743
Jason Etherton <je@example.com>
2003-09-29 14:03:05 +0000

    Added execute documentation.

--- ngat/ngtcs/command/execute/ENCLOSUREImplementor.java
+++ ngat/ngtcs/command/execute/ENCLOSUREImplementor.java
@@ -8,7 +8,7 @@ import ngat.ngtcs.command.*;
  * 
  * 
  * @author $Author: je $ 
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class ENCLOSUREImplementor extends CommandImplementor
 {
@@ -22,7 +22,7 @@ public class ENCLOSUREImplementor extends CommandImplementor
    * String used to identify RCS revision details.
    */
   public static final String rcsid =
-    new String( "$Id: ENCLOSUREImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $" );
+    new String( "$Id: ENCLOSUREImplementor.java,v 1.4 2003-09-29 14:03:05 je Exp $" );
 
   /**
    * The timeout for the ENCLOSURE command (600 seconds), in milliseconds.
@@ -60,11 +60,40 @@ public class ENCLOSUREImplementor extends CommandImplementor
 
 
   /**
-   *
+   * Close, open or move to a specified angle, the Enclosre, 1 shutter at a
+   * time.
    */
   public void execute()
   {
+    ENCLOSURE  encCmd = (ENCLOSURE)command;
+    double      angle = encCmd.getAngle();
+    TTL_Enclosure enc = TTL_Enclosure.getInstance();
 
+    try
+    {
+      // deal with shutter 1
+      if( encCmd.includesShutter1() )
+	if( encCmd.getClose() )
+	  enc.closeShutter1();
+	else if( encCmd.getOpen() )
+	  enc.openShutter1();
+	else
+	  enc.moveShutter1( angle );
+
+      // now 2
+      if( encCmd.includesShutter2() )
+	if( encCmd.getClose() )
+	  enc.closeShutter2();
+	else if( encCmd.getOpen() )
+	  enc.openShutter2();
+	else
+	  enc.moveShutter2( angle );
+	
+    }
+    catch( TTL_SystemException se )
+    {
+      logError( "Enclosure command failed : "+se.toString() );
+    }
   }
 
 
@@ -79,11 +108,14 @@ public class ENCLOSUREImplementor extends CommandImplementor
   }
 }
 /*
- *    $Date: 2003-09-26 09:58:41 $
+ *    $Date: 2003-09-29 14:03:05 $
  * $RCSfile: ENCLOSUREImplementor.java,v $
  *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/command/execute/ENCLOSUREImplementor.java,v $
- *      $Id: ENCLOSUREImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $
+ *      $Id: ENCLOSUREImplementor.java,v 1.4 2003-09-29 14:03:05 je Exp $
  *     $Log: not supported by cvs2svn $
+ *     Revision 1.3  2003/09/26 09:58:41  je
+ *     Implemented public final static TIMEOUT and public abstract int calcAcknowledgeTime()
+ *
  *     Revision 1.2  2003/09/22 13:24:36  je
  *     Added TTL TCS-Network-ICD documentation.
  *
