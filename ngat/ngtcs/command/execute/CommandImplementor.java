689a818c70954b63c2c01349b25cfc54f4ea65b4
Jason Etherton <je@example.com>
2003-09-29 11:50:28 +0000

    Added documentation.

--- ngat/ngtcs/command/execute/CommandImplementor.java
+++ ngat/ngtcs/command/execute/CommandImplementor.java
@@ -10,15 +10,27 @@ import ngat.ngtcs.command.*;
  * be implemented by the telescope control system.
  * 
  * @author $Author: je $ 
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public abstract class CommandImplementor
 {
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS FIELDS.                                                           */
+  /*                                                                         */
+  /*=========================================================================*/
+
   /**
    * String used to identify RCS revision details.
    */
   public static final String rcsid =
-    new String( "$Id: CommandImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $" );
+    new String( "$Id: CommandImplementor.java,v 1.4 2003-09-29 11:50:28 je Exp $" );
+
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT FIELDS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
 
   /**
    * Logger for each CommandImplementor.
@@ -26,27 +38,25 @@ public abstract class CommandImplementor
   protected Logger logger;
 
   /**
-   *
+   * The CommandImplementor sub-class name used for logging.  All object from
+   * the same CommandImplementor sub-class will log to the same logger.
    */
   protected String logName;
 
   /**
-   *
-   */
-  protected String name;
-
-  /**
-   *
+   * The telescope on which the command is executed
    */
   protected Telescope telescope;
 
   /**
-   *
+   * The command sub-class to execute.
    */
   protected Command command;
 
   /**
-   *
+   * A generic CommandDone object for returning the command success/failure and
+   * details.  If specific commands require a CommandDone sub-class they should
+   * be assigned in either the constructor or execute method.
    */
   protected CommandDone commandDone;
 
@@ -56,13 +66,25 @@ public abstract class CommandImplementor
    */
   protected int slept = 0;
 
-  /**
-   *
-   */
-  //  protected String returnMessage = "";
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS METHODS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
+
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT METHODS.                                                         */
+  /*                                                                         */
+  /*=========================================================================*/
 
   /**
-   *
+   * The top-level CommandImplementor constructor assigns the command to
+   * execute as well as the telescope for which it is being executed.  Also,
+   * the logging details for the concrete sub-class are assigned and a
+   * generic CommandDone is created.
+   * @param ts the telescope on which this command is to be executed
+   * @param c the command to implement execution of
    */
   public CommandImplementor( Telescope ts, Command c )
   {
@@ -75,13 +97,16 @@ public abstract class CommandImplementor
 
 
   /**
-   *
+   * The command-specific execution method.
    */
   public abstract void execute();
 
 
   /**
-   *
+   * Return the CommandDone (or sub-class) that resulted from this
+   * command implementation.
+   * @return commandDone
+   * @see #commandDone
    */
   public CommandDone getDone()
   {
@@ -90,7 +115,7 @@ public abstract class CommandImplementor
 
 
   /**
-   * Return the Telescope on which this CommandImplementor is operating.
+   * Return the Telescope on which this CommandImplementor is executing.
    * @return telescope
    */
   public Telescope getTelescope()
@@ -114,10 +139,13 @@ public abstract class CommandImplementor
   public abstract int calcAcknowledgeTime();
 }
 /*
- *    $Date: 2003-09-26 09:58:41 $
+ *    $Date: 2003-09-29 11:50:28 $
  * $RCSfile: CommandImplementor.java,v $
  *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/command/execute/CommandImplementor.java,v $
  *     $Log: not supported by cvs2svn $
+ *     Revision 1.3  2003/09/26 09:58:41  je
+ *     Implemented public final static TIMEOUT and public abstract int calcAcknowledgeTime()
+ *
  *     Revision 1.2  2003/09/22 13:24:36  je
  *     Added TTL TCS-Network-ICD documentation.
  *
