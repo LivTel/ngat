09477a7bc5ef1f358ab7a9f10aace08c9a532f50
Jason Etherton <je@example.com>
2003-10-16 15:31:25 +0000

    Implemented execute method.

--- ngat/ngtcs/command/execute/MIRROR_COVERImplementor.java
+++ ngat/ngtcs/command/execute/MIRROR_COVERImplementor.java
@@ -2,52 +2,55 @@ package ngat.ngtcs.command.execute;
 
 import ngat.ngtcs.*;
 import ngat.ngtcs.command.*;
+import ngat.ngtcs.subsystem.*;
+import ngat.ngtcs.subsystem.spt.*;
 
 /**
  * Open/Close mirror cover.
  * 
  * 
  * @author $Author: je $ 
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class MIRROR_COVERImplementor extends CommandImplementor
 {
-    /*=======================================================================*/
-    /*                                                                       */
-    /* CLASS FIELDS.                                                         */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS FIELDS.                                                           */
+  /*                                                                         */
+  /*=========================================================================*/
 
-    /**
-     * String used to identify RCS revision details.
-     */
-    public static final String rcsid =
-	new String( "$Id: MIRROR_COVERImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $" );
+  /**
+   * String used to identify RCS revision details.
+   */
+  public static final String rcsid =
+    new String( "$Id: MIRROR_COVERImplementor.java,v 1.4 2003-10-16 15:31:25 je Exp $" );
 
   /**
    * The timeout for the MIRROR_COVER command (60 seconds), in milliseconds.
    */
   public static final int TIMEOUT = 60000;
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* OBJECT FIELDS.                                                        */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT FIELDS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
 
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* CLASS METHODS.                                                        */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS METHODS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
 
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* OBJECT METHODS.                                                       */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT METHODS.                                                         */
+  /*                                                                         */
+  /*=========================================================================*/
+
   /**
    *
    */
@@ -58,10 +61,56 @@ public class MIRROR_COVERImplementor extends CommandImplementor
 
 
   /**
-   *
+   * Send the relevent command (open/close) and set the desired state
+   * accordingly.  Then wait until either the state is acheived, or the timeout
+   * is exceeded, returning a successful or failed CommandDone respectively.
    */
   public void execute()
   {
+    TTL_PrimaryMirror pm = TTL_PrimaryMirror.getInstance();
+    MIRROR_COVER mc = (MIRROR_COVER)command;
+    SPT_State desired = null, actual;
+
+    // set desired mirror cover state and send relevent command
+    try
+    {
+      if( mc.open() )
+      {
+	desired = SPT_State.E_SPT_DMD_STS_OPN;
+	pm.openCover();
+      }
+      else
+      {
+	desired = SPT_State.E_SPT_DMD_STS_CLS;
+	pm.closeCover();
+      }
+
+      // wait until timeout or succeed
+      do
+      {
+	sleep( 10000 );
+	actual = pm.getCoverState();
+      }
+      while( ( actual != desired )&&( slept < TIMEOUT ) );
+
+      // check if timeout OK
+      if( slept < TIMEOUT )
+      {
+	commandDone.setSuccessful( true );
+	return;
+      }
+
+      // timed out
+      commandDone.setErrorMessage
+	( "Primary Mirror cover did not reach desired state ["+
+	  desired.getName()+"] after "+TIMEOUT+"ms : acheived state = "+
+	  actual.getName()+"; execution terminated" );
+    }
+    catch( TTL_SystemException tse )
+    {
+      logError( tse.toString() );
+    }
+
     return;
   }
 
@@ -77,11 +126,14 @@ public class MIRROR_COVERImplementor extends CommandImplementor
   }
 }
 /*
- *    $Date: 2003-09-26 09:58:41 $
+ *    $Date: 2003-10-16 15:31:25 $
  * $RCSfile: MIRROR_COVERImplementor.java,v $
  *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/command/execute/MIRROR_COVERImplementor.java,v $
- *      $Id: MIRROR_COVERImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $
+ *      $Id: MIRROR_COVERImplementor.java,v 1.4 2003-10-16 15:31:25 je Exp $
  *     $Log: not supported by cvs2svn $
+ *     Revision 1.3  2003/09/26 09:58:41  je
+ *     Implemented public final static TIMEOUT and public abstract int calcAcknowledgeTime()
+ *
  *     Revision 1.2  2003/09/22 13:24:36  je
  *     Added TTL TCS-Network-ICD documentation.
  *
--- ngat/ngtcs/command/execute/MOVE_FOLDImplementor.java
+++ ngat/ngtcs/command/execute/MOVE_FOLDImplementor.java
@@ -2,52 +2,55 @@ package ngat.ngtcs.command.execute;
 
 import ngat.ngtcs.*;
 import ngat.ngtcs.command.*;
+import ngat.ngtcs.subsystem.*;
+import ngat.ngtcs.subsystem.amn.*;
 
 /**
  * Move fold mirror to a predetermined position.
  * 
  * 
  * @author $Author: je $ 
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class MOVE_FOLDImplementor extends CommandImplementor
 {
-    /*=======================================================================*/
-    /*                                                                       */
-    /* CLASS FIELDS.                                                         */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS FIELDS.                                                           */
+  /*                                                                         */
+  /*=========================================================================*/
 
-    /**
-     * String used to identify RCS revision details.
-     */
-    public static final String rcsid =
-	new String( "$Id: MOVE_FOLDImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $" );
+  /**
+   * String used to identify RCS revision details.
+   */
+  public static final String rcsid =
+    new String( "$Id: MOVE_FOLDImplementor.java,v 1.4 2003-10-16 15:30:36 je Exp $" );
 
   /**
    * The timeout for the MOVE_FOLD command (120 seconds), in milliseconds.
    */
   public static final int TIMEOUT = 120000;
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* OBJECT FIELDS.                                                        */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT FIELDS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
 
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* CLASS METHODS.                                                        */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS METHODS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
 
 
-    /*=======================================================================*/
-    /*                                                                       */
-    /* OBJECT METHODS.                                                       */
-    /*                                                                       */
-    /*=======================================================================*/
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT METHODS.                                                         */
+  /*                                                                         */
+  /*=========================================================================*/
+
   /**
    *
    */
@@ -62,6 +65,71 @@ public class MOVE_FOLDImplementor extends CommandImplementor
    */
   public void execute()
   {
+    MOVE_FOLD mf = (MOVE_FOLD)command;
+    int portNumber = mf.getPosition();
+    TTL_FoldMirror fm = TTL_FoldMirror.getInstance();
+    SFD_State sfdState;
+    SFP_State sfpState;
+    SFD_DeployState deployment;
+    SFP_Port port;
+
+    try
+    {
+      if( portNumber == 0 )
+      {
+	port = null; //what should this be?
+	deployment = SFD_DeployState.E_SFD_POS_STOW;
+	fm.retract();
+      }
+      else
+      {
+	port = SFP_Port.getReference( portNumber );
+	deployment = SFD_DeployState.E_SFD_POS_DEPLOY;
+	fm.deploy();
+	fm.setPortDemand( port );
+      }
+
+      do
+      {
+	sleep( 10000 );
+	sfdState = fm.get_SFD_State();
+	sfpState = fm.get_SFP_State();
+      }
+      while( ( ( sfdState != SFD_State.E_SFD_STATE_READY )||
+	       ( sfpState != SFP_State.E_SFP_STATE_READY ) )&&
+	     ( slept < TIMEOUT ) );
+
+      if( slept > TIMEOUT )
+      {
+	logError
+	  ( "Fold Mirror has not acheived desired state after "+TIMEOUT+
+	    "ms [deploy state = "+sfdState.getName()+", position state = "+
+	    sfpState.getName()+"] : execution terminated" );
+	return;
+      }
+      else if( fm.getActualPort() != port )
+      {
+	logError
+	  ( "Command has finished but actual port ["+
+	    fm.getActualPort().getName()+"] is not the desired port ["+
+	    port.getName()+"] : execution terminated" );
+	return;
+      }
+      else if( fm.getDeployState() != deployment )
+      {
+	logError
+	  ( "Command has finished but actual deployment ["+
+	    fm.getDeployState().getName()+"] is not the desired deployment ["+
+	    deployment.getName()+"] : execution terminated" );
+	return;
+      }
+    }
+    catch( TTL_SystemException tse )
+    {
+      logError( tse.toString() );
+    }
+
+    commandDone.setSuccessful( true );
     return;
   }
 
@@ -77,11 +145,14 @@ public class MOVE_FOLDImplementor extends CommandImplementor
   }
 }
 /*
- *    $Date: 2003-09-26 09:58:41 $
+ *    $Date: 2003-10-16 15:30:36 $
  * $RCSfile: MOVE_FOLDImplementor.java,v $
  *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/command/execute/MOVE_FOLDImplementor.java,v $
- *      $Id: MOVE_FOLDImplementor.java,v 1.3 2003-09-26 09:58:41 je Exp $
+ *      $Id: MOVE_FOLDImplementor.java,v 1.4 2003-10-16 15:30:36 je Exp $
  *     $Log: not supported by cvs2svn $
+ *     Revision 1.3  2003/09/26 09:58:41  je
+ *     Implemented public final static TIMEOUT and public abstract int calcAcknowledgeTime()
+ *
  *     Revision 1.2  2003/09/22 13:24:36  je
  *     Added TTL TCS-Network-ICD documentation.
  *
