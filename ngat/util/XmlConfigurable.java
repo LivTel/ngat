102c0fdc91a66fb21dd5070a62a26003c857bafa
Steve Fraser <snf@example.com>
2006-11-23 10:28:54 +0000

    Added some jdoc.

--- ngat/util/XmlConfigurable.java
+++ ngat/util/XmlConfigurable.java
@@ -2,6 +2,9 @@ package ngat.util;
 
 import org.jdom.*;
 
+/** Classes which should be configurable from a node in an XML (DOM) tree can implement
+ * this interface to allow an XmlConfigurator to configure them.
+ */
 public interface XmlConfigurable {
 
     /** Configure the object using the xml tree rooted at the supplied org.jdom.Element.
