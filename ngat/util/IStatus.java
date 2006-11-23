6a6468790d8355018bb4c924d47a87d8b0d12ffc
Steve Fraser <snf@example.com>
2006-11-23 10:26:51 +0000

    Added some jdoc comments.

--- ngat/util/IStatus.java
+++ ngat/util/IStatus.java
@@ -2,18 +2,21 @@ package ngat.util;
 
 
 /**
- * 
+ * Generic container for status results.
  */
 
 import java.io.*;
+
 /**
  * @author snf
  *
  */
 public interface IStatus extends Serializable {
 
+    /** @return the status represented by this IStatus implementation instance.*/
 	public StatusCategory getStatusCategory();
 	
+    /** @return the name of the status category identifier.*/
 	public String getCategory();
 	
 }
