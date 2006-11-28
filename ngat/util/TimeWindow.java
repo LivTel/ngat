9dfa2e0e1e5a93d22f13a76b250b93c49f7df9be
Steve Fraser <snf@example.com>
2006-11-28 10:45:37 +0000

    Initial revision

---
+++ ngat/util/TimeWindow.java
@@ -0,0 +1,15 @@
+package ngat.util;
+
+/** Temporary class for some testing - probably delete after using.*/
+public class TimeWindow {
+
+    public long start;
+
+    public long end;
+
+    public TimeWindow(long start, long end) {
+	this.start = start;
+	this.end = end;
+    }
+
+}
