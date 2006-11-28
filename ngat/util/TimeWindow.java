365058202493866c85997ea732e7c43077fc5d5d
Steve Fraser <snf@example.com>
2006-11-28 11:04:30 +0000

    Added inersect method.

--- ngat/util/TimeWindow.java
+++ ngat/util/TimeWindow.java
@@ -2,6 +2,8 @@ package ngat.util;
 
 /** Temporary class for some testing - probably delete after using.*/
 public class TimeWindow {
+    
+    public static final TimeWindow EMPTY_WINDOW = new TimeWindow(0L,0L);
 
     public long start;
 
@@ -10,6 +12,18 @@ public class TimeWindow {
     public TimeWindow(long start, long end) {
 	this.start = start;
 	this.end = end;
+    }
+
+    /** Find the intersect with another window.*/
+    public TimeWindow intersect(TimeWindow other) {
+	long is = Math.max(start, other.start);
+	long ie = Math.min(end, other.end);
+
+	if (is > ie)
+	    return EMPTY_WINDOW;
+
+	return new TimeWindow(is,ie);
+
     }
 
 }
