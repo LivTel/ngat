1d0b204cccfc57a6be41ce8ec6a0403f0079f121
Steve Fraser <snf@example.com>
2007-08-07 08:41:14 +0000

    added instane to string

--- ngat/phase2/PolarimeterConfig.java
+++ ngat/phase2/PolarimeterConfig.java
@@ -1,5 +1,5 @@
 // PolarimeterConfig.java
-// $Header: /space/home/eng/cjm/cvs/ngat/phase2/PolarimeterConfig.java,v 1.1 2006-11-20 14:51:23 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/ngat/phase2/PolarimeterConfig.java,v 1.2 2007-08-07 08:41:14 snf Exp $
 package ngat.phase2;
 
 import ngat.phase2.nonpersist.*;
@@ -18,7 +18,7 @@ public class PolarimeterConfig extends InstrumentConfig implements Serializable
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: PolarimeterConfig.java,v 1.1 2006-11-20 14:51:23 cjm Exp $"); 
+	public final static String RCSID = new String("$Id: PolarimeterConfig.java,v 1.2 2007-08-07 08:41:14 snf Exp $"); 
 	/** 
 	 * Serial version UID - used to maintain serialization compatibility
 	 * across modifications of the class's structure.
@@ -100,11 +100,14 @@ public class PolarimeterConfig extends InstrumentConfig implements Serializable
     
 
     public String toString() {
-	return "PolarimeterConfig: "+name+", Bin ["+detectors[0].getXBin()+", "+detectors[0].getYBin()+"]";
+	return "PolarimeterConfig: "+instrumentName+"/"+name+", Bin ["+detectors[0].getXBin()+", "+detectors[0].getYBin()+"]";
     }
 
 } 
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2006/11/20 14:51:23  cjm
+// Initial revision
+//
 //
 
