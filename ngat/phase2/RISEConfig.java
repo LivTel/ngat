8a2f9d1639b5aa523ac73af1059aeedefa399486
Chris Mottram <C.Mottram@ljmu.ac.uk>
2007-12-11 16:00:31 +0000

    Initial revision

---
+++ ngat/phase2/RISEConfig.java
@@ -0,0 +1,109 @@
+// RISEConfig.java
+// $Header: /space/home/eng/cjm/cvs/ngat/phase2/RISEConfig.java,v 1.1 2007-12-11 16:00:28 cjm Exp $
+package ngat.phase2;
+
+import ngat.phase2.nonpersist.*;
+
+import jyd.storable.*;
+import jyd.collection.*;
+import java.lang.reflect.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Instrument configuration for the RISE instrument. This is a simple CCD with binning configuration only.
+ */
+public class RISEConfig extends InstrumentConfig implements Serializable
+{
+	/**
+	 * Revision Control System id string, showing the version of the Class.
+	 */
+	public final static String RCSID = new String("$Id: RISEConfig.java,v 1.1 2007-12-11 16:00:28 cjm Exp $"); 
+	/** 
+	 * Serial version UID - used to maintain serialization compatibility
+	 * across modifications of the class's structure.
+	 */    
+	private static final long serialVersionUID = 7601269335374063337L;
+    
+	// Variables.
+	/**
+	 * Maxim,um number of detectors.
+	 */
+        public static final int maxDetectorCount = 1;
+
+	// Constructor.
+	/**
+	 * Default constructor.
+	 */
+	public RISEConfig()
+	{
+		this("untitled");
+	}
+    
+	/**
+	 * Constructor. Allocates detector array.
+	 */
+	public RISEConfig(String name)
+	{
+		super(name);
+		detectors = new RISEDetector[maxDetectorCount];
+		detectors[0] = new RISEDetector();
+	}
+    
+	// Accessors.
+    
+	public int getMaxDetectorCount()
+	{	
+		return maxDetectorCount; 
+	}
+
+
+	/** 
+	 * Compares with another InstConfig to see if they are effectively the same.
+	 */
+	public boolean sameAs(InstrumentConfig other)
+	{
+	    //		System.err.println("Checking PolarimterConfig with another one: "+
+	    //		   this.toString()+" with "+other.toString());
+		if (! super.sameAs(other))
+			return false;
+	
+		RISEConfig cother = (RISEConfig)other;
+
+		if (detectors[0].getXBin() != cother.getDetector(0).getXBin())
+			return false;
+		if (detectors[0].getYBin() != cother.getDetector(0).getYBin())
+			return false;
+		// Remember to look at CalBef and CalAft.
+		return true;
+	}
+
+	// Clone Constructor.
+	public NPDBObject copy()
+	{
+		try
+		{
+			return (RISEConfig)clone();
+		} 
+		catch (CloneNotSupportedException ce) 
+		{
+			return null;
+		}
+	} // end (copy).
+
+    // Formatted Text Output.
+    public void writeXml(PrintStream out, int level) {
+	out.println(tab(level)+"<RISEConfig name = '"+name+"'>");
+	detectors[0].writeXml(out,level+1);
+	out.println(tab(level)+"</RISEConfig>");
+    } // end (write).
+    
+
+    public String toString() {
+	return "RISEConfig: "+instrumentName+"/"+name+", Bin ["+detectors[0].getXBin()+", "+detectors[0].getYBin()+"]";
+    }
+
+} 
+//
+// $Log
+//
---
+++ ngat/phase2/RISEDetector.java
@@ -0,0 +1,117 @@
+// RISEDetector.java
+// $Header: /space/home/eng/cjm/cvs/ngat/phase2/RISEDetector.java,v 1.1 2007-12-11 16:00:31 cjm Exp $
+package ngat.phase2;
+
+import java.io.*;
+import java.util.*;
+import jyd.storable.*;
+import jyd.collection.*;
+import ngat.phase2.nonpersist.*;
+
+/** 
+ * Subclass of Detector to represent the RISE Detector. This is a simple CCD with binning configuration only.
+ */
+public class RISEDetector extends Detector implements Serializable
+{
+	/**
+	 * Revision Control System id string, showing the version of the Class.
+	 */
+	public final static String RCSID = new String("$Id: RISEDetector.java,v 1.1 2007-12-11 16:00:31 cjm Exp $"); 
+
+	/** The name of the detector.*/
+	public static final String name = "Andor-DV435";
+    
+	/**
+	 * Serial version UID - used to maintain serialization compatibility
+	 * across modifications of the class's structure.
+	 */
+	private static final long serialVersionUID = -941156126210593922L;   
+	
+	/**
+	 * The maximum window count: 1.
+	 */
+	public static final int maxWindowCount = 1;
+    
+	/**
+	 * The X pixel count: 1048.
+	 * Actually 1024x1024 active imaging area, but allow for bias strip.
+	 */
+	public static final int xPixelCount = 1048;
+    
+	/**
+	 * The Y pixel count: 1048.
+	 * Actually 1024x1024 active imaging area, but allow for bias strip.
+	 */
+	public static final int yPixelCount = 1048;
+	
+	/**
+	 * The maximum bin size in X axis: 8.
+	 */
+	public static final int maxXBins = 8;
+
+	/**
+	 * The maximum bin size in Y axis: 8.
+	 */
+	public static final int maxYBins = 8;
+
+	public final String getName()
+	{  
+		return name; 
+	}
+
+	/**
+	 * Subclasses should override to return the correct value. 
+	 */
+	public int getMaxDetectorCount()
+	{ 
+		return 1;
+	}
+
+	public final int getMaxWindowCount()
+	{ 
+		return maxWindowCount;
+	}
+
+	public final int getXPixelCount()
+	{
+		return xPixelCount;
+	}
+
+	public final int getYPixelCount()
+	{
+		return yPixelCount;
+	}
+    
+	public final int getMaxXBins()
+	{
+		return maxXBins;
+	}
+    
+	public final int getMaxYBins()
+	{
+		return maxYBins;
+	}
+
+	/**
+	 * Create a RISEDetector.
+	 */
+	public RISEDetector() {
+		super();
+	}
+    
+    public void writeXml(PrintStream out, int level) {
+	out.println(tab(level)+"<RISEDetector name= '"+name+"'>");
+	out.println(tab(level+1)+"<xBin>"+xBin+"</xBin>");
+	out.println(tab(level+1)+"<yBin>"+yBin+"</yBin>");
+	for (int i = 0; i < getMaxWindowCount(); i++) {
+	    if (windows[i] != null) {
+		windows[i].writeXml(out,level+1);
+	    }
+	}
+	out.println(tab(level)+"</RISEDetector>");
+    } 
+
+}
+/*
+** $Log: not supported by cvs2svn $
+*/
