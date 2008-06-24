4652f650a427a9a6e3c56f67669b28f89c9e03e9
Chris Mottram <C.Mottram@ljmu.ac.uk>
2008-06-24 15:45:21 +0000

    Added logging code.

--- ngat/df1/test/BooleanRead.java
+++ ngat/df1/test/BooleanRead.java
@@ -1,25 +1,28 @@
 // BooleanRead.java
-// $Header: /space/home/eng/cjm/cvs/ngat/df1/test/BooleanRead.java,v 1.1 2008-03-06 10:46:47 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/ngat/df1/test/BooleanRead.java,v 1.2 2008-06-24 15:45:21 cjm Exp $
 package ngat.df1.test;
 
 import java.lang.*;
 import java.io.*;
 import java.net.*;
+import java.text.*;
 import java.util.*;
 
 import ngat.df1.*;
+import ngat.util.*;
+import ngat.util.logging.*;
 
 /**
  * This class tests the Df1 library, by reading a boolean value from a PLC.
  * @author Chris Mottram
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class BooleanRead
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class
 	 */
-	public final static String RCSID = new String("$Id: BooleanRead.java,v 1.1 2008-03-06 10:46:47 cjm Exp $");
+	public final static String RCSID = new String("$Id: BooleanRead.java,v 1.2 2008-06-24 15:45:21 cjm Exp $");
 	/**
 	 * Which type of device to try to connect to.
 	 * @see ngat.df1.Df1Library#INTERFACE_DEVICE_NONE
@@ -37,6 +40,20 @@ public class BooleanRead
 	 */
 	protected int portNumber = 0;
 	/**
+	 * The logger.
+	 */
+	protected Logger logger = null;
+	/**
+	 * The filter used to filter messages sent to the logger.
+	 * @see #logger
+	 */
+	protected BitFieldLogFilter logFilter = null;
+	/**
+	 * The Df1Library log level.
+	 */
+	protected int logLevel = Df1Library.LOG_BIT_SERIAL|Df1Library.LOG_BIT_SOCKET|
+		Df1Library.LOG_BIT_DF1|Df1Library.LOG_BIT_DF1_READ_WRITE;
+	/**
 	 * The PLC address of the boolean to read. i.e. B3:0/5 , T4:0.dn.
 	 */
 	protected String plcAddress = null;
@@ -45,6 +62,7 @@ public class BooleanRead
 	 * Run method.
 	 * <ul>
 	 * <li>Creates a Df1Library instance.
+	 * <li>Calls setLogFilterLevel with the logLevel.
 	 * <li>Calls interfaceOpen with deviceId, deviceName, portNumber to connect to the PLC.
 	 * <li>Calls getBoolean with plcAddress to get the boolean value held at the specified address on the PLC.
 	 * <li>Finally, calls interfaceClose to close the connection to the PLC.
@@ -53,7 +71,9 @@ public class BooleanRead
 	 * @see #deviceId
 	 * @see #portNumber
 	 * @see #plcAddress
+	 * @see #logLevel
 	 * @see ngat.df1.Df1Library
+	 * @see ngat.df1.Df1Library#setLogFilterLevel
 	 * @see ngat.df1.Df1Library#interfaceOpen
 	 * @see ngat.df1.Df1Library#interfaceClose
 	 * @see ngat.df1.Df1Library#getBoolean
@@ -64,6 +84,7 @@ public class BooleanRead
 		boolean value;
 
 		df1 = new Df1Library();
+		df1.setLogFilterLevel(logLevel);
 		df1.interfaceOpen(deviceId,deviceName,portNumber);
 		try
 		{
@@ -77,12 +98,43 @@ public class BooleanRead
 	}
 
 	/**
+	 * Method to initialise the logger.
+	 * @see #logger
+	 * @see #logFilter
+	 */
+	protected void initLoggers()
+	{
+		LogHandler handler = null;
+		BogstanLogFormatter blf = null;
+		String loggerList[] = {"ngat.df1.Df1Library"};
+
+		// setup log formatter
+		blf = new BogstanLogFormatter();
+		blf.setDateFormat(new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss.SSS z"));
+		// setup log handler
+		handler = new ConsoleLogHandler(blf);
+		handler.setLogLevel(Logging.ALL);
+		// setup log filter
+		logFilter = new BitFieldLogFilter(Logging.ALL);
+		// Apply handler and filter to each logger in the list
+		for(int i=0;i < loggerList.length;i++)
+		{
+			System.out.println(this.getClass().getName()+":initLoggers:Setting up logger:"+loggerList[i]);
+			logger = LogManager.getLogger(loggerList[i]);
+			logger.addHandler(handler);
+			logger.setLogLevel(Logging.ALL);
+			logger.setFilter(logFilter);
+		}
+	}
+
+	/**
 	 * Parse command line arguments.
 	 * @param args The command line argument list.
 	 * @see #deviceName
 	 * @see #deviceId
 	 * @see #portNumber
 	 * @see #plcAddress
+	 * @see #logLevel
 	 * @see #help
 	 */
 	private void parseArgs(String[] args)
@@ -94,6 +146,19 @@ public class BooleanRead
 			{
 				help();
 				System.exit(0);
+			}
+			else if(args[i].equals("-log_level")||args[i].equals("-l"))
+			{
+				if((i+1)< args.length)
+				{
+					logLevel = Integer.parseInt(args[i+1]);
+					i++;
+				}
+				else
+				{
+					System.err.println("-log_level should have an numeric argument.");
+					System.exit(1);
+				}
 			}
 			else if(args[i].equals("-plc_address")||args[i].equals("-a"))
 			{
@@ -156,6 +221,7 @@ public class BooleanRead
 		System.out.println("\t-serial <device name>");
 		System.out.println("\t-socket <hostname> <port number>");
 		System.out.println("\t-plc_address <string>");
+		System.out.println("\t-log_level <number>");
 	}
 
 	/**
@@ -164,6 +230,7 @@ public class BooleanRead
 	 * <pre>java ngat.df1.test.BooleanRead -socket frodospec1serialports 3040 -plc_address B3:0/5</pre>
 	 * <ul>
 	 * <li>Calls parseArgs to parse the command line arguments.
+	 * <li>Calls initLoggers to setup logging.
 	 * <li>Calls run method to get the boolean value from the PLC.
 	 * </ul>
 	 * @param args Command line arguments.
@@ -172,6 +239,7 @@ public class BooleanRead
 	 * @see #portNumber
 	 * @see #plcAddress
 	 * @see #parseArgs
+	 * @see #initLoggers
 	 * @see #run
 	 */
 	public static void main(String[] args)
@@ -179,6 +247,7 @@ public class BooleanRead
 		BooleanRead br = new BooleanRead();
 
 		br.parseArgs(args);
+		br.initLoggers();
 		try
 		{
 			br.run();
@@ -194,4 +263,7 @@ public class BooleanRead
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2008/03/06 10:46:47  cjm
+// Initial revision
+//
 //
--- ngat/df1/test/BooleanWrite.java
+++ ngat/df1/test/BooleanWrite.java
@@ -1,25 +1,28 @@
 // BooleanWrite.java
-// $Header: /space/home/eng/cjm/cvs/ngat/df1/test/BooleanWrite.java,v 1.1 2008-03-06 10:46:47 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/ngat/df1/test/BooleanWrite.java,v 1.2 2008-06-24 15:42:37 cjm Exp $
 package ngat.df1.test;
 
 import java.lang.*;
 import java.io.*;
 import java.net.*;
+import java.text.*;
 import java.util.*;
 
 import ngat.df1.*;
+import ngat.util.*;
+import ngat.util.logging.*;
 
 /**
  * This class tests the Frodospec Df1 library, by writing a boolean value to a PLC.
  * @author Chris Mottram
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class BooleanWrite
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: BooleanWrite.java,v 1.1 2008-03-06 10:46:47 cjm Exp $");
+	public final static String RCSID = new String("$Id: BooleanWrite.java,v 1.2 2008-06-24 15:42:37 cjm Exp $");
 	/**
 	 * Which type of device to try to connect to.
 	 * @see ngat.df1.Df1Library#INTERFACE_DEVICE_NONE
@@ -37,6 +40,20 @@ public class BooleanWrite
 	 */
 	protected int portNumber = 0;
 	/**
+	 * The logger.
+	 */
+	protected Logger logger = null;
+	/**
+	 * The filter used to filter messages sent to the logger.
+	 * @see #logger
+	 */
+	protected BitFieldLogFilter logFilter = null;
+	/**
+	 * The Df1Library log level.
+	 */
+	protected int logLevel = Df1Library.LOG_BIT_SERIAL|Df1Library.LOG_BIT_SOCKET|
+		Df1Library.LOG_BIT_DF1|Df1Library.LOG_BIT_DF1_READ_WRITE;
+	/**
 	 * The PLC address of the boolean to write. i.e. B3:0/5 , T4:0.dn.
 	 */
 	protected String plcAddress = null;
@@ -49,6 +66,7 @@ public class BooleanWrite
 	 * Run method.
 	 * <ul>
 	 * <li>Creates a Df1Library instance.
+	 * <li>Calls setLogFilterLevel with the logLevel.
 	 * <li>Calls interfaceOpen with deviceId, deviceName, portNumber to connect to the PLC.
 	 * <li>Calls setBoolean with plcAddress, value to set the boolean value held at the specified address 
 	 *     on the PLC.
@@ -59,7 +77,9 @@ public class BooleanWrite
 	 * @see #portNumber
 	 * @see #plcAddress
 	 * @see #value
+	 * @see #logLevel
 	 * @see ngat.df1.Df1Library
+	 * @see ngat.df1.Df1Library#setLogFilterLevel
 	 * @see ngat.df1.Df1Library#interfaceOpen
 	 * @see ngat.df1.Df1Library#interfaceClose
 	 * @see ngat.df1.Df1Library#setBoolean
@@ -69,6 +89,7 @@ public class BooleanWrite
 		Df1Library df1 = null;
 
 		df1 = new Df1Library();
+		df1.setLogFilterLevel(logLevel);
 		df1.interfaceOpen(deviceId,deviceName,portNumber);
 		try
 		{
@@ -85,6 +106,36 @@ public class BooleanWrite
 	}
 
 	/**
+	 * Method to initialise the logger.
+	 * @see #logger
+	 * @see #logFilter
+	 */
+	protected void initLoggers()
+	{
+		LogHandler handler = null;
+		BogstanLogFormatter blf = null;
+		String loggerList[] = {"ngat.df1.Df1Library"};
+
+		// setup log formatter
+		blf = new BogstanLogFormatter();
+		blf.setDateFormat(new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss.SSS z"));
+		// setup log handler
+		handler = new ConsoleLogHandler(blf);
+		handler.setLogLevel(Logging.ALL);
+		// setup log filter
+		logFilter = new BitFieldLogFilter(Logging.ALL);
+		// Apply handler and filter to each logger in the list
+		for(int i=0;i < loggerList.length;i++)
+		{
+			System.out.println(this.getClass().getName()+":initLoggers:Setting up logger:"+loggerList[i]);
+			logger = LogManager.getLogger(loggerList[i]);
+			logger.addHandler(handler);
+			logger.setLogLevel(Logging.ALL);
+			logger.setFilter(logFilter);
+		}
+	}
+
+	/**
 	 * Parse command line arguments.
 	 * @param args The command line argument list.
 	 * @see #deviceName
@@ -93,6 +144,7 @@ public class BooleanWrite
 	 * @see #plcAddress
 	 * @see #value
 	 * @see #help
+	 * @see #logLevel
 	 */
 	private void parseArgs(String[] args)
 	{
@@ -103,6 +155,19 @@ public class BooleanWrite
 			{
 				help();
 				System.exit(0);
+			}
+			else if(args[i].equals("-log_level")||args[i].equals("-l"))
+			{
+				if((i+1)< args.length)
+				{
+					logLevel = Integer.parseInt(args[i+1]);
+					i++;
+				}
+				else
+				{
+					System.err.println("-log_level should have an numeric argument.");
+					System.exit(1);
+				}
 			}
 			else if(args[i].equals("-plc_address")||args[i].equals("-a"))
 			{
@@ -225,4 +290,7 @@ public class BooleanWrite
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2008/03/06 10:46:47  cjm
+// Initial revision
+//
 //
