1c78747514c185a3656e71d1fa45794d4f723377
The Engineer <eng@telescope.livjm.ac.uk>
2008-08-21 10:31:40 +0000

    fold posn.

--- ngat/message/RCS_TCS/MOVE_FOLD_DONE.java
+++ ngat/message/RCS_TCS/MOVE_FOLD_DONE.java
@@ -4,13 +4,14 @@ import java.util.*;
 import ngat.util.*;
 import ngat.phase2.*;
 import ngat.astrometry.*;
+import ngat.message.base.*;
 /** RCS_TCS COMMAND_DONE: MOVE_FOLD_DONE.<br>
  * Instruct TCS to move the fold mirror to a specified position.<br>
  * (These keywords may have changed).<br>
  * <dl>
  * <dt>command params: </dt>
  * <dd>state - instrument port.</dd>
- * <dd>&nbsp;values: { STOWED | POSITION1 |  POSITION2 | POSITION3 | POSITION4 }</dd>
+ * <dd>&nbsp;values: { STOWED | POSITION1-8 }</dd>
  * <dt>ack params: </dt>
  *    none.
  * <dt>done params: </dt>
@@ -25,6 +26,11 @@ public class MOVE_FOLD_DONE extends RCS_TO_TCS_DONE {
 	 */
 	public MOVE_FOLD_DONE (String id) { super(id); }
 
+	/** Returns a String representation of the object.*/
+	public String toString() {
+		return "["+this.getClass().getName()+":"+
+			getId()+"]";
+	}
 	// Hand generated code.
 
 } // class def. [MOVE_FOLD_DONE].
