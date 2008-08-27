e052ec7967eb520bfebae00222d7cb07a4d03ac3
The Engineer <eng@telescope.livjm.ac.uk>
2008-08-27 11:29:59 +0000

    Initial revision

---
+++ ngat/util/logging/DatagramLogHandler.java
@@ -0,0 +1,44 @@
+package ngat.util.logging;
+
+import java.io.*;
+import java.net.*;
+
+public class MulticastLogHandler extends LogHandler {
+
+    byte[] msg;
+
+    DatagramPacket packet;
+    
+    DatagramSocket socket;
+
+    InetAddress groupAddress;
+
+    int port;
+
+    public MulticastLogHandler(String groupName,  int port, LogFormatter formatter) throws IOException {
+	super(formatter);
+	this.port = port;	
+	socket = new DatagramSocket(0);
+	groupAddress = InetAddress.getByName(groupName);	
+	send(formatter.getHead());
+    }
+    
+    public void publish(LogRecord record) {
+	send(formatter.format(record));
+    }
+    
+    public void close() {
+	send(formatter.getTail());
+	send("BYE");
+	socket.close();
+    }
+    
+    protected void send(String message) {
+	msg = message.getBytes();
+	packet = new DatagramPacket(msg, msg.length, groupAddress, port);
+	try {
+	    socket.send(packet);	
+	} catch (IOException e ) {}
+    }
+    
+}
