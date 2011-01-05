c0b38e9a24ccdb95d9dbfd9a8c3bfc6903604f0c
Chris Mottram <C.Mottram@ljmu.ac.uk>
2011-01-05 14:27:05 +0000

    API change so class and source are logged for each log message.

--- ngat/serial/arcomess/ArcomESS.java
+++ ngat/serial/arcomess/ArcomESS.java
@@ -1,5 +1,5 @@
 // ArcomESS.java
-// $Header: /space/home/eng/cjm/cvs/ngat/serial/arcomess/ArcomESS.java,v 1.1 2009-02-04 11:26:08 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/ngat/serial/arcomess/ArcomESS.java,v 1.2 2011-01-05 14:27:05 cjm Exp $
 package ngat.serial.arcomess;
 
 import java.lang.*;
@@ -9,14 +9,14 @@ import ngat.util.logging.*;
  * This class supports an interface to a serial device, either over a 
  * serial link, or over a socket conenction via an Arcom ESS.
  * @author Chris Mottram
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ArcomESS
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: ArcomESS.java,v 1.1 2009-02-04 11:26:08 cjm Exp $");
+	public final static String RCSID = new String("$Id: ArcomESS.java,v 1.2 2011-01-05 14:27:05 cjm Exp $");
 // arcom_ess_interface.h
 	/* These constants should be the same as those in arcom_ess_interface.h */
 	/**
@@ -64,6 +64,10 @@ public class ArcomESS
 	private native void Arcom_ESS_Interface_Handle_Destroy() throws ArcomESSNativeException;
 	/**
 	 * Native wrapper to libarcom_ess routine that opens the selected interface device.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 * @param interfaceDevice The interface device to use to communicate with the PLC.
 	 * 	One of: INTERFACE_DEVICE_NONE, INTERFACE_DEVICE_SERIAL, INTERFACE_DEVICE_SOCKET.
 	 * @param deviceName The name of the device. For devices of type SERIAL, this will be something like
@@ -71,27 +75,41 @@ public class ArcomESS
 	 *        e.g. 150.204.240.115 or frodospec1serialports.
 	 * @param portNumber The port number of the SOCKET device - not used for serial devices.
 	 * @see #INTERFACE_DEVICE_NONE
-	 * @see #INTERFACE_DEVICE_TEXT
-	 * @see #INTERFACE_DEVICE_PCI
+	 * @see #INTERFACE_DEVICE_SERIAL
+	 * @see #INTERFACE_DEVICE_SOCKET
 	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if it failed.
 	 */
-	private native void Arcom_ESS_Interface_Open(int interfaceDevice,String deviceName,int portNumber)
-		throws ArcomESSNativeException;
+	private native void Arcom_ESS_Interface_Open(String clazz,String source,int interfaceDevice,String deviceName,
+						     int portNumber) throws ArcomESSNativeException;
 	/**
 	 * Native wrapper to libarcom_ess routine that closes the selected interface device.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if it failed.
 	 */
-	private native void Arcom_ESS_Interface_Close() throws ArcomESSNativeException;
+	private native void Arcom_ESS_Interface_Close(String clazz,String source) throws ArcomESSNativeException;
 	/**
 	 * Native wrapper to libarcom_ess routine that reads from the serial device.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
 	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if it failed.
 	 */
-	private native String Arcom_ESS_Interface_Read() throws ArcomESSNativeException;
+	private native String Arcom_ESS_Interface_Read(String clazz,String source) throws ArcomESSNativeException;
 	/**
 	 * Native wrapper to libarcom_ess routine that writes to the serial device.
+	 * @param clazz A string representing the class that generated log messages originiting
+	 *        from this method call. 
+	 * @param source A string representing the source that generated log messages originiting
+	 *        from this method call. 
+	 * @param s The string to write to the serial device.
 	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if it failed.
 	 */
-	private native void Arcom_ESS_Interface_Write(String s) throws ArcomESSNativeException;
+	private native void Arcom_ESS_Interface_Write(String clazz,String source,String s) 
+		throws ArcomESSNativeException;
 // arcom_ess_serial.h
 	/**
 	 * Native wrapper to libarcom_ess routine that sets the baud rate when communicating with the serial device
@@ -175,12 +193,36 @@ public class ArcomESS
 	 * @see #INTERFACE_DEVICE_SERIAL
 	 * @see #INTERFACE_DEVICE_SOCKET
 	 * @see #interfaceClose
-	 * @see #Df1_Interface_Open
 	 */
 	public void interfaceOpen(int interfaceDevice,String deviceName,int portNumber) 
 		throws ArcomESSNativeException
 	{
-		Arcom_ESS_Interface_Open(interfaceDevice,deviceName,portNumber);
+		Arcom_ESS_Interface_Open(this.getClass().getName(),null,interfaceDevice,deviceName,portNumber);
+	}
+
+	/**
+	 * Routine to open the interface. 
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @param interfaceDevice The interface device to use to communicate with the serial device.
+	 * 	One of: INTERFACE_DEVICE_NONE, INTERFACE_DEVICE_SERIAL, INTERFACE_DEVICE_SOCKET.
+	 * @param deviceName The name of the device. For devices of type SERIAL, this will be something like
+	 *        "/dev/ttyS0". For devices of type SOCKET, this should be a valid IP adddress or hostname 
+	 *        (e.g. 150.204.240.115 ot frodospec1serialports).
+	 * @param portNumber The port number of the SOCKET device - not used for serial devices.
+	 * @exception ArcomESSNativeException This method throws an ArcomESSNativeException if the device could
+	 * 	not be opened.
+	 * @see #INTERFACE_DEVICE_NONE
+	 * @see #INTERFACE_DEVICE_SERIAL
+	 * @see #INTERFACE_DEVICE_SOCKET
+	 * @see #interfaceClose
+c	 */
+	public void interfaceOpen(String clazz,String source,int interfaceDevice,String deviceName,int portNumber) 
+		throws ArcomESSNativeException
+	{
+		Arcom_ESS_Interface_Open(clazz,source,interfaceDevice,deviceName,portNumber);
 	}
 
 	/**
@@ -192,7 +234,23 @@ public class ArcomESS
 	 */
 	public void interfaceClose() throws ArcomESSNativeException
 	{
-		Arcom_ESS_Interface_Close();
+		Arcom_ESS_Interface_Close(this.getClass().getName(),null);
+	}
+
+	/**
+	 * Routine to close the interface opened with <b>interfaceOpen</b>.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if the device could
+	 * 	not be closed.
+	 * @see #interfaceOpen
+	 * @see #Arcom_ESS_Interface_Close
+	 */
+	public void interfaceClose(String clazz,String source) throws ArcomESSNativeException
+	{
+		Arcom_ESS_Interface_Close(clazz,source);
 	}
 
 	/**
@@ -204,7 +262,23 @@ public class ArcomESS
 	 */
 	public String interfaceRead() throws ArcomESSNativeException
 	{
-		return Arcom_ESS_Interface_Read();
+		return Arcom_ESS_Interface_Read(this.getClass().getName(),null);
+	}
+
+	/**
+	 * Routine to read a string from the opened interface.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @return A string read from the serial device.
+	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if the read failed.
+	 * @see #interfaceWrite
+	 * @see #Arcom_ESS_Interface_Read
+	 */
+	public String interfaceRead(String clazz,String source) throws ArcomESSNativeException
+	{
+		return Arcom_ESS_Interface_Read(clazz,source);
 	}
 
 	/**
@@ -216,7 +290,23 @@ public class ArcomESS
 	 */
 	public void interfaceWrite(String s) throws ArcomESSNativeException
 	{
-		Arcom_ESS_Interface_Write(s);
+		Arcom_ESS_Interface_Write(this.getClass().getName(),null,s);
+	}
+
+	/**
+	 * Routine to write a string to the opened interface.
+	 * @param clazz A string representing the class to put in the log record for all logs generated
+	 *              by this call.
+	 * @param source A string representing the source to put in the log record for all logs generated
+	 *              by this call.
+	 * @param s A string to write to  the serial device.
+	 * @exception ArcomESSNativeException This method throws a ArcomESSNativeException if the write failed.
+	 * @see #interfaceWrite
+	 * @see #Arcom_ESS_Interface_Read
+	 */
+	public void interfaceWrite(String clazz,String source,String s) throws ArcomESSNativeException
+	{
+		Arcom_ESS_Interface_Write(clazz,source,s);
 	}
 
 	/**
@@ -267,4 +357,7 @@ public class ArcomESS
  
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2009/02/04 11:26:08  cjm
+// Initial revision
+//
 //
