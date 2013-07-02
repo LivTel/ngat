fa727068c75f1bed3eeeb215cd457b37d4298f78
Chris Mottram <C.Mottram@ljmu.ac.uk>
2013-07-02 15:27:41 +0000

    Initial revision

---
+++ ngat/ngtcs/SubCommandExecutionThread.java
@@ -0,0 +1,117 @@
+package ngat.ngtcs;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+import ngat.util.logging.Logger;
+import ngat.ngtcs.command.*;
+import ngat.ngtcs.command.execute.*;
+
+/**
+ * This class is to look after execution of commands within commands to enable
+ * the macro commands such as CALIBRATE, and also to utilise already-written
+ * commands within larger, more complex commands such as AGMOVE compared to
+ * AGRADIAL.
+ * 
+ * @author $Author: cjm $ 
+ * @version $Revision: 1.1 $
+ */
+public class SubCommandExecutionThread extends ExecutionThread
+{
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS FIELDS.                                                           */
+  /*                                                                         */
+  /*=========================================================================*/
+
+  /**
+   * String used to identify RCS revision details.
+   */
+  public static final String rcsid =
+    new String( "$Id: SubCommandExecutionThread.java,v 1.1 2013-07-02 15:27:17 cjm Exp $" );
+
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT FIELDS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
+
+  /*=========================================================================*/
+  /*                                                                         */
+  /* CLASS METHODS.                                                          */
+  /*                                                                         */
+  /*=========================================================================*/
+
+
+  /*=========================================================================*/
+  /*                                                                         */
+  /* OBJECT METHODS.                                                         */
+  /*                                                                         */
+  /*=========================================================================*/
+
+  /**
+   * Constructor.
+   * </p><p>
+   * Create a sub-command execution thread to perform the specified command, on
+   * the specified telescope, reporting back using to the command-generator
+   * using the specified communicator.
+   * @param newTelescope the <code>Telescope</code> on which to execute
+   * @param newCommunicator the <code>Communicator</code> to use
+   * @param newCommand the <code>Command</code> to execute
+   */
+  public SubCommandExecutionThread( Telescope newTelescope,
+				    Communicator newCommunicator,
+				    Command newCommand )
+  {
+    super( newTelescope, newCommunicator, newCommand );
+  }
+
+
+  /**
+   * The <code>Thread.run</code> method.
+   * </p><p>
+   */
+  public void run()
+  {
+    try
+    {
+      instantiateImplementor();
+    }
+    catch( Exception e )
+    {
+      logger.log( 1, logName, e );
+      done = new CommandDone( command );
+      cd.setErrorMessage( e.toString() );
+      return;
+    }
+
+    logger.log( 3, logName, "executing "+command.getClass().getName()+
+		" command ["+command.getId()+"]" );
+
+    comImp.execute();
+
+    logger.log( 3, logName, "Command "+command.getClass().getName()+
+		" ["+command.getId()+"] completed" );
+
+    done = comImp.getDone();
+  }
+
+
+  /**
+   * Return the CommandDone result of the execution.
+   */
+  public CommandDone getDone()
+  {
+    return( done );
+  }
+}
+/*
+ *    $Date: 2013-07-02 15:27:17 $
+ * $RCSfile: SubCommandExecutionThread.java,v $
+ *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/SubCommandExecutionThread.java,v $
+ *      $Id: SubCommandExecutionThread.java,v 1.1 2013-07-02 15:27:17 cjm Exp $
+ *     $Log: not supported by cvs2svn $
+ *
+ */
---
+++ ngat/ngtcs/TelescopeInitialiser.java
@@ -0,0 +1,22 @@
+package ngat.ngtcs;
+
+/**
+ * 
+ * 
+ * @author $Author: cjm $ 
+ * @version $Revision: 1.1 $
+ */
+public interface TelescopeInitialiser
+{
+  /**
+   *
+   */
+  public void initialise( Telescope t ) throws InitialisationException
+}
+/*
+ *    $Date: 2013-07-02 15:27:41 $
+ * $RCSfile: TelescopeInitialiser.java,v $
+ *  $Source: /space/home/eng/cjm/cvs/ngat/ngtcs/TelescopeInitialiser.java,v $
+ *      $Id: TelescopeInitialiser.java,v 1.1 2013-07-02 15:27:41 cjm Exp $
+ *     $Log: not supported by cvs2svn $
+ */
