bb148acb4ef9b00a69466b9383af2f2e6bcb61b0
Chris Mottram <C.Mottram@ljmu.ac.uk>
2016-12-23 13:26:05 +0000

    TAIUTC from 36 to 37. UT1UTC from -0.4 to +0.6

--- ngat/astrometry/JSlalib.java
+++ ngat/astrometry/JSlalib.java
@@ -39,20 +39,23 @@ DUT1= (UT1-UTC) transmitted with time signals
 =  -0.1 seconds beginning 24 Mar 2016 at 0000 UTC 
 =  -0.2 seconds beginning 19 May 2016 at 0000 UTC  
 =  -0.3 seconds beginning 01 Sep 2016 at 0000 UTC
-=  -0.4 seconds beginning 17 Nov 2016 at 0000 UTC                 
+=  -0.4 seconds beginning 17 Nov 2016 at 0000 UTC  
+=  +0.6 seconds beginning 01 Jan 2017 at 0000 UTC        
 Beginning 1 July 2012:                                                
 TAI-UTC = 35.000 000 seconds                                       
 *  A positive leap second WILL be introduced in UTC on 30 June 2015.  *
 Beginning 1 July 2015:
 TAI-UTC = 36.000 000 seconds 
+Beginning 31 Dec 2016:
+TAI-UTC = 37.000 000 seconds 
 ***********************************************************************
 */
 
     /** TAI-UTC correction = no of leapseconds since datum.*/
-    static final double TAIUTC = 36; 
+    static final double TAIUTC = 37; 
 
     /** UT1-UTC correction from USNO Bulletin-A - this is never more than +-1.0 sec.*/
-    static final double  UT1UTC = -0.4;          
+    static final double  UT1UTC = +0.6;          
   
     /** Calls slaPlante with the specified parameters.
      * @param date The date of observation of planet as MJD.
